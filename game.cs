cce987af6331de8ba56398896be9d4923e7ef2f9
Philip Park <philip@parktribe.com>
2020-08-22 16:30:31 +1000

    Redid rpg fight.

--- game.cs
+++ game.cs
@@ -1,270 +1,150 @@
 using System;
 
-namespace Review
-{
-  class Program
-    {
 
-	static void Main(string[] args)
-	{
-      Console.WriteLine("Type in your name");
-	    string name = Console.ReadLine();
-	    string[] eDamage = new string[3]{"punch", "kick", "shoot"};
-	    int eDamageCount = eDamage.Length;
-    	string[] opponents = new string[10]{"Glitch", "Thanos", "Pac-Man", "Shredder", "Bowser", "HeroBrine", "Finn Balor", "Ganondorf", "Big Chungus", $"Dark {name}"};
-	    int opponentCount = opponents.Length;
-	    int ELife = 100;
-	    int Life = 100;
-	    int Life2 = 100;
-	    Random rnd = new Random();
-	    int opponentId = rnd.Next(opponentCount - 1);
-	    int eDamageId = rnd.Next(eDamageCount - 1);
-	    Console.WriteLine("Choose your diffuculty (Type in Easy Normal Hard or 2player)");
-	    string mode = Console.ReadLine().ToLower();
-	    if (mode == "easy"){
-		Console.WriteLine($"{name} vs {opponents[opponentId]}");
-		Console.WriteLine("Attacks are Punch, Kick and Shoot");
-		while ((ELife > 0) & (Life > 0)) {
-		    string attack = Console.ReadLine().ToLower();
-		    if (attack == "cheateasy"){
-			int damage = rnd.Next(99, 100);
-			Console.WriteLine($"You did {damage} damage!");
-			ELife = Math.Max(0, ELife - damage);
-			Console.WriteLine($"Enemys life: {ELife}");
-		    }
-		    if (attack == "punch"){
-			int damage = rnd.Next(30, 60);
-			Console.WriteLine($"You did {damage} damage!");
-			ELife = Math.Max(0, ELife - damage);
-			Console.WriteLine($"Enemys life: {ELife}");
-		    }
-		    if (attack == "kick"){
-			int damage = rnd.Next(15, 70);
-			Console.WriteLine($"You did {damage} damage!");
-			ELife = Math.Max(0, ELife - damage);
-			Console.WriteLine($"Enemys life: {ELife}");
-		    }
-		    if (attack == "shoot"){
-			int damage = rnd.Next(0, 70);
-			Console.WriteLine($"You did {damage} damage!");
-			ELife = Math.Max(0, ELife - damage);
-			Console.WriteLine($"Enemys life: {ELife}");
-		    }
-        if ( ELife  >= 1){
-		    if (eDamage[eDamageId] == "punch"){
-			int damage = rnd.Next(15, 50);
-			Console.WriteLine($"{opponents[opponentId]} {eDamage[eDamageId]}ed you");
-			Console.WriteLine($"{opponents[opponentId]} did {damage} damage to you");
-			Life = Math.Max(0, Life - damage);
-			Console.WriteLine($"Your Life: {Life}");
-		    }
-		    if (eDamage[eDamageId] == "kick"){
-			int damage = rnd.Next(5, 60);
-			Console.WriteLine($"{opponents[opponentId]} {eDamage[eDamageId]}ed you");
-			Console.WriteLine($"{opponents[opponentId]} did {damage} damage to you");
-			Life = Math.Max(0, Life - damage);
-			Console.WriteLine($"Your Life: {Life}");
-		    }
-		    if (eDamage[eDamageId] == "shoot"){
-			int damage = rnd.Next(5, 80);
-			Console.WriteLine($"{opponents[opponentId]} {eDamage[eDamageId]}ed you");
-			Console.WriteLine($"{opponents[opponentId]} did {damage} damage to you");
-			Life = Math.Max(0, Life - damage);
-			Console.WriteLine($"Your Life: {Life}");
-		    }
-      }
-        if ( Life <= 0){
-           Console.WriteLine($"{name} loses");
-           Console.WriteLine($"{opponents[opponentId]} beat you");
-            }
-            if ( ELife  <= 0){
-        		    Console.WriteLine($"{name} Wins!");
-        		    Console.WriteLine($"You beat {opponents[opponentId]}!");
-        		}
-    }
-	    }
-		 if (mode == "normal"){
-		Console.WriteLine($"{name} vs {opponents[opponentId]}");
-		Console.WriteLine("Attacks are Punch, Kick and Shoot");
-		while ((ELife > 0) & (Life > 0)) {
-		    string attack = Console.ReadLine().ToLower();
-		    if (attack == "cheatnormal"){
-			int damage = rnd.Next(99, 100);
-			Console.WriteLine($"You did {damage} damage!");
-			ELife = Math.Max(0, ELife - damage);
-			Console.WriteLine($"Enemys life: {ELife}");
-		    }
-		    if (attack == "punch"){
-			int damage = rnd.Next(25, 50);
-			Console.WriteLine($"You did {damage} damage!");
-			ELife = Math.Max(0, ELife - damage);
-			Console.WriteLine($"Enemys life: {ELife}");
-		    }
-		    if (attack == "kick"){
-			int damage = rnd.Next(10, 60);
-			Console.WriteLine($"You did {damage} damage!");
-			ELife = Math.Max(0, ELife - damage);
-			Console.WriteLine($"Enemys life: {ELife}");
-		    }
-		    if (attack == "shoot"){
-			int damage = rnd.Next(0, 70);
-			Console.WriteLine($"You did {damage} damage!");
-			ELife = Math.Max(0, ELife - damage);
-			Console.WriteLine($"Enemys life: {ELife}");
-		    }
-        if ( ELife  >= 1){
-		    if (eDamage[eDamageId] == "punch"){
-			int damage = rnd.Next(25, 50);
-			Console.WriteLine($"{opponents[opponentId]} {eDamage[eDamageId]}ed you");
-			Console.WriteLine($"{opponents[opponentId]} did {damage} damage to you");
-			Life = Math.Max(0, Life - damage);
-			Console.WriteLine($"Your Life: {Life}");
-		    }
-		    if (eDamage[eDamageId] == "kick"){
-			int damage = rnd.Next(10, 60);
-			Console.WriteLine($"{opponents[opponentId]} {eDamage[eDamageId]}ed you");
-			Console.WriteLine($"{opponents[opponentId]} did {damage} damage to you");
-			Life = Math.Max(0, Life - damage);
-			Console.WriteLine($"Your Life: {Life}");
-		    }
-		    if (eDamage[eDamageId] == "shoot"){
-			int damage = rnd.Next(0, 70);
-			Console.WriteLine($"{opponents[opponentId]} {eDamage[eDamageId]}ed you");
-			Console.WriteLine($"{opponents[opponentId]} did {damage} damage to you");
-			Life = Math.Max(0, Life - damage);
-			Console.WriteLine($"Your Life: {Life}");
-		    }
-      }
-        if ( Life <= 0){
-           Console.WriteLine($"{name} loses");
-           Console.WriteLine($"{opponents[opponentId]} beat you");
-            }
-            if ( ELife  <= 0){
-        		    Console.WriteLine($"{name} Wins!");
-        		    Console.WriteLine($"You beat {opponents[opponentId]}!");
-        		}
-    }
-		 }
-		 if (mode == "hard"){
-		Console.WriteLine($"{name} vs {opponents[opponentId]}");
-		Console.WriteLine("Attacks are Punch, Kick and Shoot");
-		while ((ELife > 0) & (Life > 0)) {
-		    string attack = Console.ReadLine().ToLower();
-		    if (attack == "cheathard"){
-			int damage = rnd.Next(99, 100);
-			Console.WriteLine($"You did {damage} damage!");
-			ELife = Math.Max(0, ELife - damage);
-			Console.WriteLine($"Enemys life: {ELife}");
-		    }
-		    if (attack == "punch"){
-			int damage = rnd.Next(25, 50);
-			Console.WriteLine($"You did {damage} damage!");
-			ELife = Math.Max(0, ELife - damage);
-			Console.WriteLine($"Enemys life: {ELife}");
-		    }
-		    if (attack == "kick"){
-			int damage = rnd.Next(10, 60);
-			Console.WriteLine($"You did {damage} damage!");
-			ELife = Math.Max(0, ELife - damage);
-			Console.WriteLine($"Enemys life: {ELife}");
-		    }
-		    if (attack == "shoot"){
-			int damage = rnd.Next(0, 70);
-			Console.WriteLine($"You did {damage} damage!");
-			ELife = Math.Max(0, ELife - damage);
-			Console.WriteLine($"Enemys life: {ELife}");
-		    }
-        if ( ELife  >= 1){
-		    if (eDamage[eDamageId] == "punch"){
-			int damage = rnd.Next(30, 60);
-			Console.WriteLine($"{opponents[opponentId]} {eDamage[eDamageId]}ed you");
-			Console.WriteLine($"{opponents[opponentId]} did {damage} damage to you");
-			Life = Math.Max(0, Life - damage);
-			Console.WriteLine($"Your Life: {Life}");
-		    }
-		    if (eDamage[eDamageId] == "kick"){
-			int damage = rnd.Next(15, 70);
-			Console.WriteLine($"{opponents[opponentId]} {eDamage[eDamageId]}ed you");
-			Console.WriteLine($"{opponents[opponentId]} did {damage} damage to you");
-			Life = Math.Max(0, Life - damage);
-			Console.WriteLine($"Your Life: {Life}");
-		    }
-		    if (eDamage[eDamageId] == "shoot"){
-			int damage = rnd.Next(50, 100);
-			Console.WriteLine($"{opponents[opponentId]} {eDamage[eDamageId]}ed you");
-			Console.WriteLine($"{opponents[opponentId]} did {damage} damage to you");
-			Life = Math.Max(0, Life - damage);
-			Console.WriteLine($"Your Life: {Life}");
-		    }
-      }
-        if ( Life <= 0){
-           Console.WriteLine($"{name} loses");
-           Console.WriteLine($"{opponents[opponentId]} beat you");
-            }
-            if ( ELife  <= 0){
-                Console.WriteLine($"{name} Wins!");
-                Console.WriteLine($"You beat {opponents[opponentId]}!");
-            }
-    }
-		 }
-		 if (mode == "2player"){
-        Console.WriteLine("Player 2 type in your name");
-		     string name2 = Console.ReadLine();
-		     Console.WriteLine($"{name} vs {name2}");
-		     while ((Life2 > 0) & (Life > 0)) {
-		     Console.WriteLine($"{name}'s turn");
-		     string attack = Console.ReadLine().ToLower();
-		     if (attack == "punch"){
-			 int damage = rnd.Next(15, 50);
-			 Console.WriteLine($"{name} did {damage} damage");
-			 Life2 = Math.Max(0, Life2 - damage);
-			 Console.WriteLine($"{name2}'s life: {Life2}");
-		     }
-		     if (attack == "kick"){
-			 int damage = rnd.Next(10, 60);
-			 Console.WriteLine($"{name} did {damage} damage");
-			 Life2 = Math.Max(0, Life2 - damage);
-			 Console.WriteLine($"{name2}'s life: {Life2}");
-		     }
-		     if (attack == "shoot"){
-			 int damage = rnd.Next(5, 80);
-			 Console.WriteLine($"{name} did {damage} damage");
-			 Life2 = Math.Max(0, Life2 - damage);
-			 Console.WriteLine($"{name2}'s life: {Life2}");
-		     }
-         if ( Life2  >= 1){
-		     Console.WriteLine($"{name2}'s turn");
-         string attack2 = Console.ReadLine().ToLower();
-		     if (attack2 == "punch"){
-			 int damage = rnd.Next(15, 50);
-			 Console.WriteLine($"{name2} did {damage} damage");
-			 Life = Math.Max(0, Life - damage);
-			 Console.WriteLine($"{name}'s Life: {Life}");
-		     }
-		     if (attack2 == "kick"){
-			 int damage = rnd.Next(10, 60);
-			 Console.WriteLine($"{name2} did {damage} damage");
-			 Life = Math.Max(0, Life - damage);
-			 Console.WriteLine($"{name}'s Life: {Life}");
-		     }
-		     if (attack2 == "shoot"){
-			 int damage = rnd.Next(5, 80);
-			 Console.WriteLine($"{name2} did {damage} damage");
-			 Life = Math.Max(0, Life - damage);
-			 Console.WriteLine($"{name}'s Life: {Life}");
-		     }
-         if ( Life <= 0){
-            Console.WriteLine($"{name2} wins!");
-            Console.WriteLine($"You beat {name}!");
-             }
-           }
-           if ( Life2  <= 0){
-               Console.WriteLine($"{name} Wins!");
-               Console.WriteLine($"You beat {name2}!");
-           }
-		     }
-		 }
-		Console.WriteLine("Made by TriPhorceJ");
-	}
+namespace review
+{
+class Player
+{
+  Mode pMode;
+
+  Random rnd;
+  string name;
+
+  int Life = 100;
+  public Player(string playerName) {
+    name = playerName;
+    rnd = new Random();
+  }
+  public void assignMode(Mode m) {
+    pMode = m;
+  }
+  private int assignDamage(int damage) {
+    //Console.WriteLine($"You did {damage} damage!");
+    //ELife = Math.Max(0, ELife - damage);
+    //Console.WriteLine($"Enemys life: {ELife}");
+    return(damage);
+  }
+  public int health() {
+    return(Life);
+  }
+  public string Name() {
+    return(name);
+  }
+  public void takeDamage(int damage) {
+    //Console.WriteLine($"{opponents[opponentId]} {eDamage[eDamageId]}ed you");
+    //Console.WriteLine($"{opponents[opponentId]} did {damage} damage to you");
+    Life = Math.Max(0, Life - damage);
+    //Console.WriteLine($"Your Life: {Life}");
+  }
+  public int kick() {
+    int damage = rnd.Next(pMode.kick[0], pMode.kick[1]);
+    return(assignDamage(damage));
+  }
+  public int punch() {
+    int damage = rnd.Next(pMode.punch[0], pMode.punch[1]);
+    return(assignDamage(damage));
+  }
+  public int shoot() {
+    int damage = rnd.Next(pMode.shoot[0], pMode.shoot[1]);
+    return(assignDamage(damage));
   }
 }
+class Mode
+{
+  public int[] shoot;
+  public int[] punch;
+  public int[] kick;
+
+  public Mode() {
+
+  }
+
+  public void strong() {
+    shoot = new int[] { 5, 80 };
+    punch = new int[] { 30, 60 };
+    kick = new int[] { 15, 70 };
+  }
+  public void weak() {
+    kick = new int[] { 5, 60 };
+    punch = new int[] { 15, 50 };
+    shoot = new int[] { 0, 70 };
+  }
+}
+
+
+class Program
+{
+
+  static void playerStatus(Player player1, Player player2) {
+    Console.WriteLine($"{player1.Name()}: {player1.health()} | {player2.Name()}: {player2.health()}");
+  }
+
+  static void attackPhase(Player attacker, Player defender) {
+
+    Console.Write($"{attacker.Name()}: Type in your attack (punch, kick, shoot): ");
+    string attackType = Console.ReadLine().ToLower();
+
+    switch (attackType){
+      case "shoot":
+        defender.takeDamage(attacker.shoot());
+        break;
+      case "punch":
+        defender.takeDamage(attacker.punch());
+        break;
+      case "kick":
+        defender.takeDamage(attacker.kick());
+        break;
+      default:
+        Console.Write($"Attack unknown. You missed.");
+        break;
+    }
+  }
+
+  static Mode setMode(Player player){
+
+    Mode playerMode = new Mode();
+
+    Console.Write($"{player.Name()}: Select your mode (weak or strong): ");
+    string mode = Console.ReadLine().ToLower();
+    switch (mode){
+      case "strong":
+        playerMode.strong();
+        break;
+        case "weak":
+      playerMode.weak();
+      break;
+    }
+
+    return(playerMode);
+  }
+
+
+  static void Main(string[] args)
+  {
+    //string[] eDamage = new string[3]{"punch", "kick", "shoot"};
+    //int eDamageCount = eDamage.Length;
+    //int eDamageId = rnd.Next(eDamageCount - 1);
+
+    Console.Write("Type in yor name: ");
+    string name = Console.ReadLine();
+
+    Player player1 = new Player(name);
+    Mode player1Mode = setMode(player1);
+    player1.assignMode(player1Mode);
+
+    Player player2 = new Player("Bob");
+    Mode player2Mode = setMode(player2);
+    player2.assignMode(player2Mode);
+
+    while ((player2.health() > 0) & (player1.health() > 0)) {
+
+      attackPhase(player1, player2);
+      playerStatus(player1, player2);
+      if (player2.health() <= 0){
+      attackPhase(player2, player1);
+      playerStatus(player1, player2);
+    }
+    }
+  }
+  }
+  }
